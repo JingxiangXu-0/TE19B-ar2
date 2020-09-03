7276b8a755d55741a2d5a2f339b2f75cb8289892
Jing Xu <jingxiang.xu@learnet.se>
2020-09-03 11:06:01 +0200

    Övat på redigera, escape, kommentarer

---
+++ Uppgift 1-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_1_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Detta är en enkelsrad kommentar... tryck bara på ctrl + Q
+
+      /*
+      Tryck på ctrl + shift + Q
+      */
+      /*      Console.WriteLine("Detta är ett program gjort i C#.");
+           Console.WriteLine("Programmet skriver ut text på flera rader.");
+           Console.WriteLine("Tryck på valfri tangent för att avsluta programmet.");
+      */
+      Console.WriteLine("Detta är ett program gjort i C#. \n" + "Programmet skriver ut text på flera rader. \n" + "Tryck på valfri tangent för att avsluta programmet.");
+    }
+  }
+}
