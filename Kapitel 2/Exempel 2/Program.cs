132e79a3cf75305d4eb4f80f2fca3a133cd8a4db
Jing Xu <jingxiang.xu@learnet.se>
2020-09-10 11:15:20 +0200

    Vi jobbade med Double, Int och etc

---
+++ Kapitel 2/Exempel 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exempel_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Deklarera variiabel
+      string namn = "";
+      int ålder = 0; //Ingen semicolon i int
+      int ålderÅr2025 = 0; //camelCase
+      double längd = 0;
+      string meddelande = "";
+
+
+      //   Läs in namnet
+      Console.Write("Vad heter du? ");
+      namn = Console.ReadLine();
+
+      //   Läs in längd i meter
+      Console.Write("Hur lång är du? ");
+      längd = double.Parse(Console.ReadLine());
+
+      //   Läs in ålder
+      Console.Write("Vad gammal är du? ");
+      ålder = int.Parse(Console.ReadLine());
+
+      //   Hur gammal är användaren 2020
+      ålderÅr2025 = ålder + 5;
+
+      // Resultatet
+      meddelande = "Hej " + namn + ", du är " + längd + "m lång, och blir " + ålderÅr2025 + " år gammal om 2025.";
+
+      Console.WriteLine(meddelande);
+    }
+  }
+}
---
+++ Kapitel 2/Uppgift 2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hur många meter kan du hoppa i höjdhopp?");
+
+      double minHopp = double.Parse(Console.ReadLine());
+      double skillnaden = 2.45 - minHopp;
+
+      Console.WriteLine("Hej, världsrekordet är " + skillnaden + " meter högre. ya suck");
+    }
+  }
+}
