32d04e5bae6c61c30b1c635b7674d5eb4993ed86
Jing Xu <jingxiang.xu@learnet.se>
2020-09-16 14:57:06 +0200

    Övat på varibler och inmatningar.

---
+++ Kapitel 2/Uppgift 2-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_2_4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int tal1 = int.Parse(args[0]);
+      int tal2 = int.Parse(args[1]);
+
+      //   Console.WriteLine("Skriv ett tal!");
+      //   double tal1 = double.Parse(Console.ReadLine());
+
+      //   Console.WriteLine("Yani, Skriv ett annat tal!");
+      //   double tal2 = double.Parse(Console.ReadLine());
+
+      double summan = tal1 + tal2;
+      double produkt = tal1 * tal2;
+      Console.WriteLine("Summan av talen är " + summan + " och produkten är " + produkt + "!");
+    }
+  }
+}
---
+++ Kapitel 2/Uppgift-2-5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_2_5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Vilket år är det?");
+      double årNu = double.Parse(Console.ReadLine());
+
+      double årKvar = 2100 - årNu;
+      Console.WriteLine("Det är " + årKvar + " kvar tills det blir år 2100. :)");
+    }
+  }
+}
