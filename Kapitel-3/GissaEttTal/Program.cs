09ea0b0eab3c622e611d07b64c6f695a1b91c46b
Jing Xu <jingxiang.xu@learnet.se>
2020-10-01 11:06:13 +0200

    Träna på if och loopar

---
+++ Kapitel-3/GissaEttTal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GissaEttTal
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      int randomNummer = random.Next(1, 7);
+
+      for (int i = 0; i < 3; i++)
+      {
+        Console.WriteLine("Gissa ett tal (1-6)? ");
+        int gissning = int.Parse(Console.ReadLine());
+        if (gissning == randomNummer)
+        {
+          Console.WriteLine("Du har rätt!");
+          i = 5;
+        }
+        else
+        {
+          Console.WriteLine("Du har fel, försök igen");
+        }
+
+      }
+    }
+  }
+}
