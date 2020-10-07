341d8dbe3773515deb4f5750c9e086ca1cc42ec2
Jing Xu <jingxiang.xu@learnet.se>
2020-10-07 14:58:40 +0200

    Switch

---
+++ Kapitel-4/Exempel-1-2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exempel_1_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int val = 0;
+
+      while (val != 3)
+      {
+        Console.WriteLine("\nVälj ett av alternativen.");
+        Console.WriteLine("1. Addera två tal");
+        Console.WriteLine("2. Multiplicera två tal");
+        Console.WriteLine("3. Avsluta programmet");
+        Console.Write("Jag väljer alternativ : ");
+        break;
+      }
+
+      val = int.Parse(Console.ReadLine());
+
+      switch (val)
+      {
+        case 1:
+          Console.WriteLine("Addera talen = ");
+          Double tal1 = double.Parse(Console.ReadLine());
+          Double tal2 = double.Parse(Console.ReadLine());
+          Console.WriteLine(tal1 + " + " + tal2 + " = " + (tal1 + tal2));
+          break;
+
+        case 2:
+          Console.WriteLine("multiplicera talen = ");
+          Double faktor1 = double.Parse(Console.ReadLine());
+          Double faktor2 = double.Parse(Console.ReadLine());
+          Console.WriteLine(faktor1 + " * " + faktor2 + " = " + (faktor1 * faktor2));
+          break;
+
+
+        case 3:
+          Console.WriteLine("Byeee");
+          break;
+
+        default:
+          Console.Write("Error");
+          break;
+      }
+    }
+  }
+}
---
+++ Kapitel-4/Exempel-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exempel_1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      int randomNummer = random.Next(1, 7);
+      int försök = 3;
+
+      while (försök >= 0)
+      {
+        Console.WriteLine("Gissa ett tal (1-6)? ");
+        int gissning = int.Parse(Console.ReadLine());
+
+
+        if (gissning == randomNummer)
+        {
+          Console.WriteLine("Du har rätt!");
+          break;
+        }
+        else
+        {
+          Console.WriteLine("Du har fel! Försök igen. Du har " + försök + " kvar.");
+          försök--;
+        }
+
+      }
+    }
+  }
+}
