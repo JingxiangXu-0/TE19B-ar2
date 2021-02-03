ee94c1519b24a411de9fd5135d82bcae2b8ad2f5
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-02-03 14:44:47 +0100

    Update Program.cs

--- Kapitel-5/Uppgift-5-13/Program.cs
+++ Kapitel-5/Uppgift-5-13/Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 
-namespace Uppgift_5_13
+namespace Uppgift_5._13
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hur många heltal vill du skriva?");
+      Console.WriteLine("Hur många heltal vill du skriva in?");
       int antal = int.Parse(Console.ReadLine());
 
-      int[] Heltaler = new int[antal];
+      int[] talLista = new int[antal];
 
       for (int i = 0; i < antal; i++)
       {
         Console.WriteLine("Skriv in ett heltal");
-        Heltaler[i] = int.Parse(Console.ReadLine());
+        talLista[i] = int.Parse(Console.ReadLine());
       }
 
       Random slump = new Random();
-      int i = slump.Next(0, Heltaler.Length);
+      int index = slump.Next(0, talLista.Length);
 
-      Console.WriteLine("Talet med index " + i + " har värdet " + Heltaler[slump]);
+      Console.WriteLine($"Talet med index {index} har värdet {talLista[index]}");
 
     }
   }
