c12fcf6114baf3aca1ba9a6293c6e53cc13714b5
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-02-03 14:42:10 +0100

    filhantering
    niceee

--- Projekt/Bordshantering/Program.cs
+++ Projekt/Bordshantering/Program.cs
@@ -7,30 +7,31 @@ namespace Projekt_2
   {
     static void Main(string[] args)
     {
-      string[] bordsInformation;
+      string[] bordsInnehåll;
       string filnamn = "jingsbord.txt";
-      string tomtBordBeskrivning = "0;Inga gäster";
-      int antalbord = 5;
+      string tomtBord = "0;Inga gäster";
+      int antalbord = 20;
 
       Console.WriteLine("Detta är Jings bordshanterare\nBordsinformation lästes in från fil");
 
       // kollar om filen finns
       if (File.Exists(filnamn))
       {
-        string bordsInnehåll = File.ReadAllText(filnamn);
+        bordsInnehåll = File.ReadAllLines(filnamn);
         Console.WriteLine("Här är Bords infromationen:");
         Console.WriteLine(bordsInnehåll);
       }
       else //om filen inte finns, skapas en ny fil
       {
-        bordsInformation = new string[antalbord];
-        for (int i = 0; i < bordsInformation.Length; i++)
+        bordsInnehåll = new string[antalbord];
+        for (int i = 0; i < bordsInnehåll.Length; i++)
         {
-          bordsInformation[i] = tomtBordBeskrivning;
+          bordsInnehåll[i] = tomtBord;
         }
-        File.WriteAllLines(filnamn, bordsInformation);
+        File.WriteAllLines(filnamn, bordsInnehåll);
         Console.WriteLine("Fil med bordsinformation fanns ej så ny fil skapades");
       }
+      // Tom rad innan det körs
       Console.WriteLine();
 
       //  Programmets Meny
@@ -38,67 +39,86 @@ namespace Projekt_2
       int menuVal = 0;
       while (menuVal != 4)
       {
-        Console.WriteLine("Välj ett av alternativen.");
+        Console.WriteLine("\n\nVälj ett av alternativen.");
         Console.WriteLine("1. Visa alla bord");
         Console.WriteLine("2. Lägg ändra bordsinformation");
         Console.WriteLine("3. Markera att ett bord är tomt");
         Console.WriteLine("4. Avsluta programmet");
-        Console.Write("Jag väljer alternativ : ");
+        Console.Write("\nJag väljer alternativ : ");
         menuVal = int.Parse(Console.ReadLine());
-        break;
-      }
 
-      switch (menuVal)
-      {
-        case 1:
-          // om det är inte personer i ett bord
-          int totaltAntalGäster = 0;
+        Console.WriteLine();
 
-          for (int i = 0; i < bordsInformation.Length; i++)
-          {
-            if (bordsInformation[i] == tomtBordBeskrivning)
+        switch (menuVal)
+        {
+          case 1:
+            // om det är inte personer i ett bord
+            int totaltAntalGäster = 0;
+            for (int i = 0; i < bordsInnehåll.Length; i++)
             {
-              Console.WriteLine($"Bord {i + 1} - Inga gäster");
-              continue;
+              if (bordsInnehåll[i] == tomtBord)
+              {
+                Console.WriteLine($"Bord {i + 1} - Inga gäster");
+                continue;
+              }
+
+              // Detta sker bara om bordet inte är tomt
+              string[] specefikBord = bordsInnehåll[i].Split(';');
+              int antalGäst = int.Parse(specefikBord[0]);
+              string bordsnamn = specefikBord[1];
+              totaltAntalGäster += antalGäst;
+              Console.WriteLine($"Bord {i + 1} - Namn: {bordsnamn}, antal gäster: {antalGäst}");
+
             }
-            // om det är personer vid ett bord
-            string[] speciellBordsinformation = bordsInformation[i].Split(';');
-            int antalGäster = int.Parse(speciellBordsinformation[0]);
-            string bordsnamn = speciellBordsinformation[1];
-            totaltAntalGäster += antalGäster;
-            Console.WriteLine($"Bord {i + 1} - Namn: {bordsnamn}, Antal gäster: {antalGäster}");
-
-          }
-          Console.WriteLine($"Totalt antal gäster: {totaltAntalGäster}");
-          break;
-
-        case 2:
-          Console.WriteLine("Vilket bordsnummer vill du lägga till/ändra informationen för?");
-          int ändraBordsnummer = int.Parse(Console.ReadLine());
-
-          while (ändraBordsnummer <= 0 || ändraBordsnummer > bordsInformation.Length)
-          {
-            Console.WriteLine($"{ändraBordsnummer} är inte ett giltigt bordsnummer. Försök igen.");
+            Console.WriteLine($"\nTotalt antal gäster: {totaltAntalGäster}");
             break;
-          }
 
-          Console.WriteLine("Skriv in bordets namn");
-          string bordsNamn = Console.ReadLine();
-          Console.WriteLine("Hur många gäster finns vid bordet?");
-          int antalGäster = int.Parse(Console.ReadLine());
-          break;
+          case 2:
+            Console.WriteLine("Vilket bordsnummer vill du lägga till/ändra informationen för?");
+            int ändraBordsnummer = int.Parse(Console.ReadLine());
 
-        case 3:
+            while (ändraBordsnummer <= 0 || ändraBordsnummer > bordsInnehåll.Length)
+            {
+              Console.WriteLine($"{ändraBordsnummer} är inte ett giltigt bordsnummer. Försök igen.");
+              break;
+            }
 
-          break;
+            string[] BordInfoNy = new string[2];
+            Console.WriteLine("Hur många gäster finns vid bordet?");
+            BordInfoNy[0] = Console.ReadLine();
+            Console.WriteLine("Skriv in Bordets namn");
+            BordInfoNy[1] = Console.ReadLine();
+            bordsInnehåll[ändraBordsnummer - 1] = string.Join(";", BordInfoNy);
 
-        case 4:
-          Console.WriteLine("Hejdå!");
-          break;
+            // Uppdatera sparfilen
+            File.WriteAllLines(filnamn, bordsInnehåll);
+            break;
 
-        default:
-          Console.WriteLine("error");
-          break;
+          case 3:
+            Console.WriteLine("Vilket bordsnummer ska vara tomt?");
+            int bordsnummerRadera = int.Parse(Console.ReadLine());
+            if (bordsnummerRadera <= 0 || bordsnummerRadera > bordsInnehåll.Length)
+            {
+              Console.WriteLine($"{bordsnummerRadera} är inte ett giltigt bordsnummer");
+              break;
+            }
+
+            bordsInnehåll[bordsnummerRadera - 1] = tomtBord;
+            Console.WriteLine($"Bord {bordsnummerRadera} är markerat som tomt");
+
+            // Uppdatera sparfilen
+            File.WriteAllLines(filnamn, bordsInnehåll);
+            break;
+
+          case 4:
+            Console.WriteLine("Hejdå!");
+            break;
+
+          default:
+            Console.WriteLine("error");
+            break;
+
+        }
       }
     }
   }
